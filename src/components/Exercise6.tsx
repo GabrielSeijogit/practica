abda8eded18d6afc43e06a2c0a3d288f1ae2670d
Emilse Botta <emilsebotta.cde@gmail.com>
2024-01-16 14:55:18 -0300

    ejercicio-6 resuelto

--- src/components/Exercise6.tsx
+++ src/components/Exercise6.tsx
@@ -1,15 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+
+interface Productos {
+  id: number;
+  producto: string;
+  precio: number;
+}
 
 const carrito = [
-    { id: 1, producto: "Laptop", precio: 1000 },
-    { id: 2, producto: "Mouse", precio: 20 },
-    { id: 3, producto: "Teclado", precio: 50 },
-  ];
-  
+  { id: 1, producto: "Laptop", precio: 1000 },
+  { id: 2, producto: "Mouse", precio: 20 },
+  { id: 3, producto: "Teclado", precio: 50 },
+];
 
 export const Exercise6 = () => {
+  const [discountValue, setDiscountValue] = useState<number>(0);
+  const [productsDiscount, setProductDiscount] = useState<Productos[]>(carrito);
+
+  const discount = () => {
+    const final = carrito.map((p) => {
+      const x = p.precio - (p.precio * discountValue) / 100;
+      return { id: p.id, producto: p.producto, precio: x };
+    });
+
+    setProductDiscount(final);
+  };
 
   return (
-    <div>Resolver aca ejercicio 6</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        padding: 10,
+        justifyContent: "space-between",
+        flexDirection: "column",
+      }}
+    >
+      <p>Lista de Productos</p>
+      <div>
+        {productsDiscount.map((p) => (
+          <p>
+            {p.producto} ${p.precio}
+          </p>
+        ))}
+      </div>
+      <div>
+        <input
+          style={{ color: "black" }}
+          type="number"
+          value={discountValue}
+          onChange={(e) => setDiscountValue(e.target.valueAsNumber)}
+        />
+        <button
+          className="h-10 px-6 ml-2  rounded-md border  text-slate-900"
+          onClick={discount}
+        >
+          Descontar
+        </button>
+      </div>
+    </div>
+  );
+};
